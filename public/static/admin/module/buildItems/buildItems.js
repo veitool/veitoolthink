fdf675c109d67a3e744305f6b17302b978d2ad78
veitool <26843818@qq.com>
2023-05-16 17:52:59 +0800

    修复buildItems构建器图片删除后验证提示

--- public/static/admin/module/buildItems/buildItems.js
+++ public/static/admin/module/buildItems/buildItems.js
@@ -66,13 +66,13 @@ layui.define(['tagsInput','fileLibrary','cascader'], function(e){
     //TinyMCE编辑器
     c.tinymce = c.item_html + c.block_html + '>' + c.textarea_html + ' id="tinymce-{{ d.id ? d.id : d.name }}">{{ d.value }}</textarea>' + c.tips_html + '</div></div>';
     //上传单图
-    c.image = c.item_html + c.block_html + '><div id="image-show-{{ d.name }}" data-type="image" data-verify="{{ d.verify || \'\' }}">{{# if (d.value) { }}' +
+    c.image = c.item_html + c.block_html + '><div id="image-show-{{ d.name }}" data-type="image" data-verify="{{ d.verify || \'\' }}" data-reqtext="{{ d.reqtext || \'\' }}">{{# if (d.value) { }}' +
               '<div class="imgs_item"><img src="{{ d.value }}"/><input type="hidden" name="{{ d.name }}" value="{{ d.value }}"/>'+
               '<i class="layui-icon layui-icon-edit item-edit"></i><i class="layui-icon layui-icon-close item-delete"></i></div>{{# }else{ }}<input type="hidden" name="{{ d.name }}" lay-verify="{{ d.verify || \'\' }}" lay-reqtext="{{ d.reqtext || \'\' }}"/>{{# } }}</div>'+
               '<div class="imgs_item layui-upload-drag" style="background:#efefef;" id="up-image-btn-{{ d.name }}" data-type="image" data-name="{{ d.name }}" data-thum="{{ d.thum || 0 }}"><i class="layui-icon">&#xe67c;</i><br/>上传图片</div>'+
               '<div style="clear:both"></div>' + c.tips_html + '</div></div>';
     //上传多图
-    c.images = c.item_html + c.block_html + '><div id="image-show-{{ d.name }}" data-type="images" data-verify="{{ d.verify || \'\' }}">{{# var i = 0;layui.each(d.value, function(key, url){ i=1; }}'+
+    c.images = c.item_html + c.block_html + '><div id="image-show-{{ d.name }}" data-type="images" data-verify="{{ d.verify || \'\' }}" data-reqtext="{{ d.reqtext || \'\' }}">{{# var i = 0;layui.each(d.value, function(key, url){ i=1; }}'+
               '<div class="imgs_item"><img src="{{ url }}"/><input type="hidden" name="{{ d.name }}[]" value="{{ url }}"/>'+
               '<i class="layui-icon layui-icon-edit item-edit"></i><i class="layui-icon layui-icon-close item-delete"></i></div>{{# }); }}{{# if (i == 0) { }}<input type="hidden" name="{{ d.name }}" lay-verify="{{ d.verify || \'\' }}" lay-reqtext="{{ d.reqtext || \'\' }}"/>{{# } }}</div>'+
               '<div class="imgs_item layui-upload-drag" style="background:#efefef;" id="up-image-btn-{{ d.name }}" data-type="images" data-name="{{ d.name }}" data-thum="{{ d.thum || 0 }}"><i class="layui-icon">&#xe67c;</i><br/>上传图片</div>'+
@@ -296,7 +296,7 @@ layui.define(['tagsInput','fileLibrary','cascader'], function(e){
                     $this.parent().remove();
                     if(pparent.children().length==0){
                         let name = pparent.attr('id').replace("image-show-","");
-                        pparent.html('<input type="hidden" name="'+ name +'" lay-verify="'+ pparent.data('verify') +'"/>');
+                        pparent.html('<input type="hidden" name="'+ name +'" lay-verify="'+ pparent.data('verify') +'" lay-reqtext="'+ pparent.data('reqtext') +'"/>');
                     }
                     layer.close(index);
                 });

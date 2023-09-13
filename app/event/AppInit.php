60953c001d1df4e34d7861f2b6cfdc16dd66e810
veitool <26843818@qq.com>
2023-09-13 21:50:53 +0800

    还原操作

--- app/event/AppInit.php
+++ app/event/AppInit.php
@@ -21,12 +21,36 @@ class AppInit extends Service
 {
     public function handle()
     {
+        // 版本信息
+        define('VT_VERSION', '1.0.3');
+        // 后台管理员 session 标识
+        define('VT_MANAGER', 'V_MANAGER');
+        // 前台会员 session 标识
+        define('VT_MEMBER', 'V_MEMBER');
+        // 时间戳
+        define('VT_TIME', time());
+        // 分隔符
+        define('VT_DS', DIRECTORY_SEPARATOR);
+        // 项目目录
+        define('ROOT_PATH', $this->app->getRootPath());
+        // 插件目录
+        define('ADDON_PATH', ROOT_PATH . 'addons' . VT_DS);
+        // 临时目录
+        define('RUNTIME_PATH', ROOT_PATH . 'runtime' . VT_DS);
+        // 站点目录
+        define('VT_PUBLIC', ROOT_PATH . 'public' . VT_DS);
+        // IP地址
+        define('VT_IP', $this->app->request->ip());
+        // 资源目录 运行目录为根目录时请设为空\其他二级或多级目录时后面无斜杠如：/mydir 或 /mydir/xyz
+        define('VT_DIR', config('view.tpl_replace_string.{PUBLIC__PATH}'));
+
         /* 路由处理 旧模式不兼容nginx：$url = request()->pathinfo(); */
         $url = str_replace(".".config('route.url_html_suffix'), '', ltrim($this->app->request->url(), '/'));
         $url = strpos($url, '?') ? strstr($url, '?', true) : $url;
         $arr = explode('/', $url);
         $addon = $arr[0];
         $module = '';
+
         /* 插件应用处理 */
         if(in_array($addon,config('veitool.addons'))){
             $this->app->config->set(['app_express'=>false], 'app');
@@ -39,6 +63,7 @@ class AppInit extends Service
             Route::rule($url, $module . '/' . $contr . '/' . $method);
             $module .= '/';
         }/**/
+
         /* 插件应用名 用于AdminBase.php中兼容插件权限 */
         define('ADDON_APP', $module);
     }

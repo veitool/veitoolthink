257d6105558d111b65c4f1c4f217185896f155ac
veitool <26843818@qq.com>
2024-05-20 09:53:23 +0800

    增加Redis配置

--- config/cache.php
+++ config/cache.php
@@ -24,5 +24,28 @@ return [
             'serialize'  => [],
         ],
         // 更多的缓存连接
+        // redis缓存
+        'redis' => [
+            // 驱动方式
+            'type'          => 'redis',
+            // 服务器地址
+            'host'          => env('redis.hostname', '127.0.0.1'),
+            // 端口
+            'port'          => (int)env('redis.hostport', '6379'),
+            // 密码
+            'password'      => env('redis.password', ''),
+            // 缓存有效期 0表示永久缓存
+            'expire'        => 0,
+            // 缓存前缀
+            'prefix'        => '',
+            // 缓存标签前缀
+            'tag_prefix'    => 'tag:',
+            // 数据库 0号数据库
+            'select'        => (int)env('redis.select', 0),
+            // 序列化机制 例如 ['serialize', 'unserialize']
+            'serialize'     => [],
+            // 服务端超时
+            'timeout'       => 0
+        ],
     ],
 ];

990434f85a15ac504798b579908ce1867738627a
veitool <26843818@qq.com>
2024-05-26 19:38:05 +0800

    增加swoole静态访问路由

---
+++ route/swoole.php
@@ -0,0 +1,12 @@
+<?php
+use think\facade\Route;
+
+Route::get('static/:path', function (string $path) {
+    $filename = public_path() .'static/'. $path;
+    return new \think\swoole\response\File($filename);
+})->pattern(['path' => '.*\.\w+$']);
+
+Route::get('favicon.ico', function () {
+    $filename = public_path() .'favicon.ico';
+    return new \think\swoole\response\File($filename);
+})->pattern(['path' => '.*\.\w+$']);

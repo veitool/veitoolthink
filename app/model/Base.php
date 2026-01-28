03cc9aa68f376bfa35576181e85e8b649bbb304d
veitool <26843818@qq.com>
2026-01-28 18:22:38 +0800

    修正模型基类删除方法

--- app/model/Base.php
+++ app/model/Base.php
@@ -78,40 +78,35 @@ class Base extends Model
     }
 
     /**
-     * 删除数据 (无须返回删除数量，可调用 Model::destroy($id) 进行删除)
-     * @param  mixed  $where   查询条件 
-     * 软删模式开启时：可传入主键值:1/'1' 或:主键值数组[1,2]/['1','2'] 或 键值对:['id'=>'1'] 或 闭包:function($query)use($id){$query->where("id=$id");}
-     * 软删模式关闭时：可传入字符串条件(需软删模式关闭):"id = $id" 或 键值对:['id'=>'1'] 或 二维数组条件[['id','=',1]] 或 闭包
-     * @param  bool   $soft    软删模式 默认:关闭 (模型在启用了软删时要真实删除数据并返回删除的数量，必须开启该模式才有效)
-     * @param  bool   $force   是否真删 默认:真实删除
+     * 删除数据【返回删除条数】(若无须返回删除数量则可用 Model::destroy($id) 或闭包 Model::destroy(function($query){$query->where('id','>',10);}); 进行删除)
+     * @param  mixed $where 可是键值:1/'1' 或 主键值数组[1,2]/['1','2'] 或 键值对:['id'=>'1'] 或 闭包:function($query)use($id){$query->where("id=$id");} 或字符串查询条件
+     * @param  bool  $force 是否真删 默认:软删除(前提是模型中引用了：use \think\model\concern\SoftDelete;即开启了软删)，设为true则不受限真实删除
      * @return int 删除的记录数
      */
-    public static function del($where, bool $soft = false, bool $force = true)
+    public static function del(mixed $where, bool $force = false)
     {
-        if($soft && !is_string($where)){
-            if(empty($where) && 0 !== $where){ // 传入空值（包括空字符串和空数组）的时候不会做任何的数据删除操作，但传入0则是有效的
-                return false;
-            }
-            $query = (new static())->db();
-            if($force){
-                $query->removeOption('soft_delete');
-            }
-            if(is_array($where) && key($where) !== 0){ //键值对形式
-                $query->where($where);
-                $where = [];
-            }elseif ($where instanceof \Closure){ //闭包形式
-                call_user_func_array($where, [ &$query]);
-                $where = [];
-            }
-            $result = $query->select((array) $where);
-            $i = 0;
-            foreach($result as $rs){
-                $i += $rs->force($force)->delete();
-            }
-            return $i;
-        }else{
-            return $force ? self::where($where)->delete() : self::where($where)->useSoftDelete('del_time',time())->delete();
+        if(empty($where) && 0 !== $where){ // 传入空值（包括空字符串和空数组）的时候不会做任何的数据删除操作，但传入0则是有效的
+            return false;
         }
+
+        $query = (new static())->db();
+        if($force){
+            $query->removeOption('soft_delete');
+        }
+        if((is_string($where) && strpos($where, ' ') !== false) || (is_array($where) && key($where) !== 0)){ //查询字符串 或 键值对形式
+            $query->where($where);
+            $where = [];
+        }elseif ($where instanceof \Closure){ //闭包形式
+            call_user_func_array($where, [ &$query]);
+            $where = [];
+        }
+
+        $result = $query->select((array)$where);
+        $i = 0;
+        foreach($result as $rs){
+            $i += $rs->force($force)->delete();
+        }
+        return $i;
     }
 
     /**
